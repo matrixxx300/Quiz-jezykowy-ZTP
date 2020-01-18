de28b74b66454145173fe963b8ce4ad97d324824
billcosby <t.szeligowski@onet.pl>
2020-01-18 17:23:52 +0100

    Wyrównany tekst

--- Quiz/src/main/java/controller/question/QuestionController.java
+++ Quiz/src/main/java/controller/question/QuestionController.java
@@ -4,6 +4,7 @@ import controller.QuizSelectController;
 import controller.level.LearnQuizLevelSelectController;
 import javafx.fxml.FXML;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 import model.progress.Progress;
 import model.question.ClosedQuestion;
